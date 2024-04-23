b005bfba7e94d55ecd32c5f44fb08b78a6951cb4
v-wangke3 <v-wangke3@360.cn>
2024-04-23 23:38:37 +0800

    fix: 修复没有选中样式

--- source/.vitepress/config/sidebar.js
+++ source/.vitepress/config/sidebar.js
@@ -5,12 +5,12 @@ export default {
   "/": [
     {
       text: "Base",
-      collapsed: true, // 初始不折叠
+      collapsed: false, // 初始不折叠
       items: [
         { text: "技术清单", link: "/view/Js/home" },
         { text: "Array的方法简单整理", link: "/view/Js/array" },
-        { text: "Object的方法简单整理", link: "view/Js/object" },
-        { text: "String的方法简单整理", link: "view/Js/string" },
+        { text: "Object的方法简单整理", link: "/view/Js/object" },
+        { text: "String的方法简单整理", link: "/view/Js/string" },
         { text: "Date时间", link: "/view/Js/date" },
         { text: "RegExp正则", link: "/view/Js/reg" },
         { text: "CSS使用锦囊", link: "/view/Css/css" },
@@ -58,44 +58,44 @@ export default {
           collapsed: true,
           sidebarDepth: 4,
           items: [
-            { text: "vue基本用法", link: "view/Frame/vue/vue" },
+            { text: "vue基本用法", link: "/view/Frame/vue/vue" },
             // { text: "会用到的插件", link: "view/Frame/vue/use-project" },
-            { text: "vue中使用JSX", link: "view/Frame/vue/jsx" },
-            { text: "脚手架vue-cli", link: "view/Frame/vue/vue-cli" },
-            { text: "问题验证解析", link: "view/Frame/vue/problem" },
-            { text: "Vue预渲染", link: "view/Frame/vue/prerender" },
+            { text: "vue中使用JSX", link: "/view/Frame/vue/jsx" },
+            { text: "脚手架vue-cli", link: "/view/Frame/vue/vue-cli" },
+            { text: "问题验证解析", link: "/view/Frame/vue/problem" },
+            { text: "Vue预渲染", link: "/view/Frame/vue/prerender" },
             {
               text: "踩坑记录",
               collapsed: true,
-              items: [{text: "使用keepAlive遇到的坑",link: "view/Frame/vue/keep-alive"}]
+              items: [{text: "使用keepAlive遇到的坑",link: "/view/Frame/vue/keep-alive"}]
             },
             {
               text: "使用拓展",
               collapsed: true,
               items: [
-                {text: "vue之JSX封装table",link: "view/Frame/vue/table-jsx"},
-                {text: "Element-UI ，Table组件实现拖拽效果",link: "view/Frame/vue/sortable"},
-                {text: "Vue之JSX封装搜索头部",link: "view/Frame/vue/with-search"},
-                {text: "封装树形菜单",link: "view/Frame/vue/tree-menu"},
-                {text: "基于Vue的组件局部刷新",link: "view/Frame/vue/reload"}
+                {text: "vue之JSX封装table",link: "/view/Frame/vue/table-jsx"},
+                {text: "Element-UI ，Table组件实现拖拽效果",link: "/view/Frame/vue/sortable"},
+                {text: "Vue之JSX封装搜索头部",link: "/view/Frame/vue/with-search"},
+                {text: "封装树形菜单",link: "/view/Frame/vue/tree-menu"},
+                {text: "基于Vue的组件局部刷新",link: "/view/Frame/vue/reload"}
               ]
             },
             {
               text: "源码分析",
               collapsed: true,
               items: [
-                {text: "vue3.0源码分析-1",link: "view/Frame/vue/next/reactivity"},
-                {text: "Vue3.0源码分析-2",link: "view/Frame/vue/vue-next"},
-                {text: "Vue之JSX封装搜索头部",link: "view/Frame/vue/error-handler"},
-                {text: "封装树形菜单",link: "view/Frame/vue/source-code/keep-alive"},
-                {text: "基于Vue的组件局部刷新",link: "view/Frame/vue/source-code/vue-extend"},
+                {text: "vue3.0源码分析-1",link: "/view/Frame/vue/next/reactivity"},
+                {text: "Vue3.0源码分析-2",link: "/view/Frame/vue/vue-next"},
+                {text: "Vue之JSX封装搜索头部",link: "/view/Frame/vue/error-handler"},
+                {text: "封装树形菜单",link: "/view/Frame/vue/source-code/keep-alive"},
+                {text: "基于Vue的组件局部刷新",link: "/view/Frame/vue/source-code/vue-extend"},
               ]
             },
             {
               text: "UI组件库",
               collapsed: true,
               items: [
-                {text: "irdd 组件库",link: "view/Frame/irdd/irdd"}
+                {text: "irdd 组件库",link: "/view/Frame/irdd/irdd"}
               ]
             }
           ]
@@ -105,18 +105,18 @@ export default {
           text: '☞ react',
           collapsed: true,
           items: [
-            {text: "React基本用法",link: "view/Frame/react/react"},
-            {text: "Hooks",link: "view/Frame/react/react-hooks"},
-            {text: "Immer",link: "view/Frame/react/immer"},
+            {text: "React基本用法",link: "/view/Frame/react/react"},
+            {text: "Hooks",link: "/view/Frame/react/react-hooks"},
+            {text: "Immer",link: "/view/Frame/react/immer"},
             // {text: "",link: "view/Frame/react/reactComponent"},
-            {text: "Router原理",link: "view/Frame/react/react-router"},
-            {text: "Redux源码解析",link: "view/Frame/react/react-redux"},
-            {text: "Dva封装",link: "view/Frame/react/dva"},
-            {text: "react事件委托机制",link: "view/Frame/react/event"},
-            {text: "React16 Fiber",link: "view/Frame/react/fiber"},
-            {text: "VirtualDOM",link: "view/Frame/react/dom_diff"},
-            {text: "React-SSR",link: "view/Frame/react/ssr"},
-            {text: "问题验证解析",link: "view/Frame/react/problem"}
+            {text: "Router原理",link: "/view/Frame/react/react-router"},
+            {text: "Redux源码解析",link: "/view/Frame/react/react-redux"},
+            {text: "Dva封装",link: "/view/Frame/react/dva"},
+            {text: "react事件委托机制",link: "/view/Frame/react/event"},
+            {text: "React16 Fiber",link: "/view/Frame/react/fiber"},
+            {text: "VirtualDOM",link: "/view/Frame/react/dom_diff"},
+            {text: "React-SSR",link: "/view/Frame/react/ssr"},
+            {text: "问题验证解析",link: "/view/Frame/react/problem"}
           ]
         },
 
@@ -125,9 +125,9 @@ export default {
           collapsed: true,
           items: [
             // {text: "Taro基本用法",link: "view/Frame/taro/taroBase"},
-            {text: "Taro一些配置",link: "view/Frame/taro/taroAdvanced"},
-            {text: "Node生成模板",link: "view/Frame/taro/template"},
-            {text: "项目目录",link: "view/Frame/taro/toc"}
+            {text: "Taro一些配置",link: "/view/Frame/taro/taroAdvanced"},
+            {text: "Node生成模板",link: "/view/Frame/taro/template"},
+            {text: "项目目录",link: "/view/Frame/taro/toc"}
           ]
         },
 
@@ -135,9 +135,9 @@ export default {
           text: '☞ react-native',
           collapsed: true,
           items: [
-            {text: "RN基本用法",link: "view/Frame/react-native/guide"},
-            {text: "RN IOS环境搭建",link: "view/Frame/react-native/base"},
-            {text: "XCODE模拟器报错修复",link: "view/Frame/react-native/bug"}
+            {text: "RN基本用法",link: "/view/Frame/react-native/guide"},
+            {text: "RN IOS环境搭建",link: "/view/Frame/react-native/base"},
+            {text: "XCODE模拟器报错修复",link: "/view/Frame/react-native/bug"}
           ]
         },
 
@@ -145,7 +145,7 @@ export default {
           text: '☞ 单元测试',
           collapsed: true,
           items: [
-            {text: "单元测试框架Jest学习总结",link: "view/Frame/unit-test/jest"}
+            {text: "单元测试框架Jest学习总结",link: "/view/Frame/unit-test/jest"}
           ]
         }
 
@@ -160,11 +160,11 @@ export default {
           text: '☞ NodeJS',
           collapsed: true,
           items: [
-            { text: "NodeJs的原生方法", link: "view/Server/node/base" },
-            { text: "Koa2", link: "view/Server/node/node-1" },
-            { text: "NodeJs 插件拓展", link: "view/Server/node/node-2" },
-            { text: "Node启动工具", link: "view/Server/node/pm2" },
-            { text: "终端实现console输出不同颜色", link: "view/Server/node/node-console" }
+            { text: "NodeJs的原生方法", link: "/view/Server/node/base" },
+            { text: "Koa2", link: "/view/Server/node/node-1" },
+            { text: "NodeJs 插件拓展", link: "/view/Server/node/node-2" },
+            { text: "Node启动工具", link: "/view/Server/node/pm2" },
+            { text: "终端实现console输出不同颜色", link: "/view/Server/node/node-console" }
           ]
         },
 
@@ -172,8 +172,8 @@ export default {
           text: '☞ Mongoose',
           collapsed: true,
           items: [
-            { text: "Mongodb安装", link: "view/Server/sql/mongodb" },
-            { text: "Mongoose使用", link: "view/Server/sql/mongoose" }
+            { text: "Mongodb安装", link: "/view/Server/sql/mongodb" },
+            { text: "Mongoose使用", link: "/view/Server/sql/mongoose" }
           ]
         },
 
@@ -181,13 +181,13 @@ export default {
           text: '☞ Nginx',
           collapsed: true,
           items: [
-            { text: "Mongodb安装", link: "view/Server/nginx/nginx_base" },
-            { text: "Mongoose使用", link: "view/Server/nginx/nginx_local" }
+            { text: "Mongodb安装", link: "/view/Server/nginx/nginx_base" },
+            { text: "Mongoose使用", link: "/view/Server/nginx/nginx_local" }
           ]
         },
 
-        { text: "云服务器配置", link: "view/Server/lnmp" },
-        { text: "什么是cookie,token和session?它们之间有什么关系？", link: "view/Server/token" }
+        { text: "云服务器配置", link: "/view/Server/lnmp" },
+        { text: "什么是cookie,token和session?它们之间有什么关系？", link: "/view/Server/token" }
       ]
     },
 
@@ -195,9 +195,9 @@ export default {
       text: "TypeScript",
       collapsed: true, // 初始不折叠
       items: [
-        { text: "TypeScript", link: "view/Typescript/ts" },
-        { text: "TypeScr 误区", link: "view/Typescript/ts-object-type-interface.md" },
-        { text: "tsconfig了解一下", link: "view/Typescript/tsConfig" },
+        { text: "TypeScript", link: "/view/Typescript/ts" },
+        { text: "TypeScr 误区", link: "/view/Typescript/ts-object-type-interface.md" },
+        { text: "tsconfig了解一下", link: "/view/Typescript/tsConfig" },
         // { text: "", link: "view/Typescript/use" },
       ]
     },
@@ -210,9 +210,9 @@ export default {
           text: '☞ webpack',
           collapsed: true,
           items: [
-            { text: "Webpack4 配置详解", link: "view/Webpack/webpack" },
-            { text: "Webpack的使用", link: "view/Webpack/use" },
-            { text: "Babel-polyfill", link: "view/Webpack/babel_polyfill" }
+            { text: "Webpack4 配置详解", link: "/view/Webpack/webpack" },
+            { text: "Webpack的使用", link: "/view/Webpack/use" },
+            { text: "Babel-polyfill", link: "/view/Webpack/babel_polyfill" }
           ]
         },
 
@@ -220,7 +220,7 @@ export default {
           text: '☞ gulp',
           collapsed: true,
           items: [
-            { text: "gulp", link: "view/Gulp/base" }
+            { text: "gulp", link: "/view/Gulp/base" }
           ]
         },
 
@@ -228,7 +228,7 @@ export default {
           text: '☞ rollup',
           collapsed: true,
           items: [
-            { text: "rollup", link: "view/Rollup/base" }
+            { text: "rollup", link: "/view/Rollup/base" }
           ]
         },
 
@@ -236,7 +236,7 @@ export default {
           text: '☞ snowpack',
           collapsed: true,
           items: [
-            { text: "rollup", link: "view/Snowpack/base" }
+            { text: "rollup", link: "/view/Snowpack/base" }
           ]
         },
 
@@ -244,7 +244,7 @@ export default {
           text: '☞ vite',
           collapsed: true,
           items: [
-            { text: "vite", link: "view/Vite/base" }
+            { text: "vite", link: "/view/Vite/base" }
           ]
         }
       ]
@@ -254,10 +254,10 @@ export default {
       text: "Git教程",
       collapsed: true, // 初始不折叠
       items: [
-        { text: "git基础", link: "view/Git/git" },
-        { text: "git恢复上一次的修改", link: "view/Git/git-cherry" },
-        { text: "git hooks", link: "view/Git/git-hooks" },
-        { text: "github API 调用", link: "view/Git/github" }
+        { text: "git基础", link: "/view/Git/git" },
+        { text: "git恢复上一次的修改", link: "/view/Git/git-cherry" },
+        { text: "git hooks", link: "/view/Git/git-hooks" },
+        { text: "github API 调用", link: "/view/Git/github" }
       ]
     },
 
@@ -265,12 +265,12 @@ export default {
       text: "Other",
       collapsed: true, // 初始不折叠
       items: [
-        { text: "Jenkins自动化部署", link: "view/Jenkins/base" },
-        { text: "MAC整理", link: "tools/mac" },
-        { text: "开发工具", link: "tools/store" },
-        { text: "网站大全", link: "tools/page" },
-        { text: "配置WS", link: "other/ws-configure" },
-        { text: "习题解答", link: "view/Interviews/result" },
+        { text: "Jenkins自动化部署", link: "/view/Jenkins/base" },
+        { text: "MAC整理", link: "/tools/mac" },
+        { text: "开发工具", link: "/tools/store" },
+        { text: "网站大全", link: "/tools/page" },
+        { text: "配置WS", link: "/other/ws-configure" },
+        { text: "习题解答", link: "/view/Interviews/result" },
       ]
     }
   ]
