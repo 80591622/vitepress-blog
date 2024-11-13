c38cf395459a068352dc2264fe71e40a598cff10
v-wangke3 <v-wangke3@360.cn>
2024-11-13 18:54:22 +0800

    服务器迁移修改脚本文件

--- deploy.sh
+++ deploy.sh
@@ -18,15 +18,15 @@ else
     tar -zcvf dist.tar.gz "C:/Users/v-wangke3/Desktop/vitepress-blog/dist" || { echo "打包失败"; exit 1; }
 fi
 
-# 上传到服务器（需要输入密码，如果已经进行过私钥配置，则不用），其中/home/wwwapp/www.wkdevhub.cn 为上传文件所在目录
-scp  -r dist.tar.gz root@101.42.135.167:/home/wwwapp/www.wkdevhub.cn || { echo "上传失败"; exit 1; }
+# 上传到服务器（需要输入密码，如果已经进行过私钥配置，则不用），其中/www/wwwroot/www.wkdevhub.cn 为上传文件所在目录
+scp  -r dist.tar.gz root@129.204.108.97:/www/wwwroot/www.wkdevhub.cn || { echo "上传失败"; exit 1; }
 
 # 登录到服务器（需要输入密码，如果已经进行过私钥配置，则不用）
 # 服务器环境开启
-ssh root@101.42.135.167 << SSH_COMMANDS
+ssh root@129.204.108.97 << SSH_COMMANDS
 
 # 进入目标目录
-cd /home/wwwapp/www.wkdevhub.cn || { echo "进入目标目录失败"; exit 1; }
+cd /www/wwwroot/www.wkdevhub.cn || { echo "进入目标目录失败"; exit 1; }
 
 # 解压
 tar -zxvf dist.tar.gz --strip-components=5 -C dist || { echo "解压失败"; exit 1; }
